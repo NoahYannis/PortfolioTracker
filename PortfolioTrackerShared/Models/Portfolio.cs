a5df833040a5e8212e77133905c26800f73612b0
NoahYannis <noah.guderjahn@gmx.de>
2023-07-24 22:40:00 +0200

    Expanded Portfolio Model

--- PortfolioTrackerShared/Models/Portfolio.cs
+++ PortfolioTrackerShared/Models/Portfolio.cs
@@ -1,35 +1,36 @@
-﻿namespace PortfolioTrackerShared.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PortfolioTrackerShared.Models
 {
     public class Portfolio
     {
         /// <summary>
         /// All stocks inside the user's portfolio
         /// </summary>
+        /// 
+
+        [Key]
+        public int Id { get; set; }
         public List<PortfolioStock> Positions { get; set; } = new();
         public List<Order> OrderHistory { get; set; } = new();
+
+        /// <summary>
+        /// Portfolio Owner
+        /// </summary>
+        public User User { get; set; } = new();
+
+
+        /// <summary>
+        /// Portfolio Name
+        /// </summary>
         public string Name { get; set; } = string.Empty;
-        public decimal TotalValue { get; set; } = decimal.Zero;
 
-        #region Portfolio Performance
+        /// <summary>
+        /// Total value
+        /// </summary>
+        public decimal Value { get; set; } = decimal.Zero;
+        public decimal TotalRelativePerfomance { get; set; } = decimal.Zero;
+        public decimal TotalAbsolutePerformance { get; set; } = decimal.Zero;
 
-        public decimal? RelativePerformance
-        {
-            get
-            {
-                return Positions.Any() ? Positions.Sum(stock => stock.RelativePerformance) : decimal.Zero;
-            }
-            set { }
-        }
-
-        public decimal? AbsolutePerformance
-        {
-            get
-            {
-                return Positions.Any() ? Positions.Sum(stock => stock.AbsolutePerformance) : decimal.Zero;
-            }
-            set { }
-        }
-
-        #endregion
     }
 }
