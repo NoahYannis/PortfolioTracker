620ae0abb62d95dba50cc51d2fcb0c7172e5f740
Noah Yannis Guderjahn <77248099+NoahYannis@users.noreply.github.com>
2023-08-28 22:07:25 +0200

    user settings

---
+++ PortfolioTrackerShared/Models/UserModels/UserSettings.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PortfolioTrackerShared.Models.UserModels
+{
+	/// <summary>
+	/// Settings defined by the user
+	/// </summary>
+	public class UserSettings
+	{
+		public Color ColorScheme { get; set; } = Color.Purple; // Todo: Implement color wheel
+
+		/// <summary>
+		/// Investing goals (portfolio value, dividend income per month...)
+		/// </summary>
+		public List<string> InvestingGoals { get; set; } = new List<string>();
+
+		public string PreferedLanguage { get; set; } = "en";
+	}
+}
