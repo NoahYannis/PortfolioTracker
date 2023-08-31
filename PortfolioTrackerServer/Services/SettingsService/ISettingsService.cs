5d8f70005ffe5766ea970f1cbf593bb7f520c703
Noah Yannis Guderjahn <77248099+NoahYannis@users.noreply.github.com>
2023-08-31 22:50:34 +0200

    implemented usersettings service on the server

--- PortfolioTrackerServer/Services/SettingsService/ISettingsService.cs
+++ PortfolioTrackerServer/Services/SettingsService/ISettingsService.cs
@@ -1,7 +1,13 @@
-﻿namespace PortfolioTrackerServer.Services.SettingsService
+﻿using PortfolioTrackerShared.Models.UserModels;
+using PortfolioTrackerShared.Other;
+
+namespace PortfolioTrackerServer.Services.SettingsService
 {
 	public interface ISettingsService
 	{
+		Task<ServiceResponse<UserSettings>> GetUserSettings(User user);
+		Task<bool> ResetUserSettings(User user);
+		Task<bool> UpdateUserSettings(User user, UserSettings newSettings);
 
 	}
 }
--- PortfolioTrackerServer/Services/SettingsService/SettingsService.cs
+++ PortfolioTrackerServer/Services/SettingsService/SettingsService.cs
@@ -1,7 +1,76 @@
-﻿namespace PortfolioTrackerServer.Services.SettingsService
+﻿using Microsoft.EntityFrameworkCore;
+using PortfolioTrackerServer.Data;
+using PortfolioTrackerShared.Models.UserModels;
+using PortfolioTrackerShared.Other;
+
+namespace PortfolioTrackerServer.Services.SettingsService
 {
 	public class SettingsService : ISettingsService
 	{
+		private readonly DataContext _dataContext;
+
+		public SettingsService(DataContext dataContext)
+		{
+			_dataContext = dataContext;
+		}
+
+		/// <summary>
+		/// Return settings for a given user.
+		/// </summary>
+		/// <param name="user"></param>
+		/// <returns></returns>
+		public async Task<ServiceResponse<UserSettings>> GetUserSettings(User user)
+		{
+			// User whose settings we want
+			var userSettings = await _dataContext.UserSettings.FirstOrDefaultAsync(us => us.UserId == user.UserId);
+
+			// If found return the corresponding user`s settings or 
+			return userSettings is not null ? new ServiceResponse<UserSettings> { Data = userSettings }
+				: new ServiceResponse<UserSettings>
+				{
+					Success = false,
+					Message = $"No settings found for User '{user.UserName}'"
+				};
+		}
+
+		/// <summary>
+		/// Updates a user's settings.
+		/// </summary>
+		/// <param name="user"></param>
+		/// <returns></returns>
+		public async Task<bool> UpdateUserSettings(User user, UserSettings newSettings)
+		{
+			var existingSettings = await _dataContext.UserSettings.FirstOrDefaultAsync(us => us.UserId == user.UserId);
+
+			if (existingSettings is null)
+				return false;
+
+			_dataContext.UserSettings.Remove(existingSettings);
+			_dataContext.UserSettings.Add(newSettings);
+
+			await _dataContext.SaveChangesAsync(); // Save the changes to the database
+
+			return true;
+		}
+
+		/// <summary>
+		/// Resets a user's settings to default values.
+		/// </summary>
+		/// <param name="user"></param>
+		/// <returns></returns>
+		public async Task<bool> ResetUserSettings(User user)
+		{
+			var existingSettings = await _dataContext.UserSettings.FirstOrDefaultAsync(us => us.UserId == user.UserId);
+
+			if (existingSettings is null)
+				return false;
+
+			_dataContext.UserSettings.Remove(existingSettings);
+			_dataContext.UserSettings.Add(new UserSettings { UserId = user.UserId });
+			await _dataContext.SaveChangesAsync();
+
+			return true;
+		}
 
 	}
 }
