2907253caaac4de34e239cbe37663b72aa457127
Noah Yannis Guderjahn <77248099+NoahYannis@users.noreply.github.com>
2023-09-10 21:09:44 +0200

    translation

--- PortfolioTrackerClient/Program.cs
+++ PortfolioTrackerClient/Program.cs
@@ -10,11 +10,11 @@ global using PortfolioTrackerClient.Services.PortfolioService;
 global using PortfolioTrackerShared.Models.UserModels;
 global using PortfolioTrackerShared.Other;
 using PortfolioTrackerClient.Services.SettingsService;
+using Microsoft.Extensions.Localization;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
-
 builder.Services.AddBlazoredLocalStorage();
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
@@ -27,5 +27,9 @@ builder.Services.AddBlazoredModal();
 builder.Services.AddOptions();
 builder.Services.AddAuthorizationCore();
 builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthStateProvider>();
+builder.Services.AddScoped<IStringLocalizer<App>, StringLocalizer<App>>();
+//builder.Services.AddLocalization();
+
+
 
 await builder.Build().RunAsync();
