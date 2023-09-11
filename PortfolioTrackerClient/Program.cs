b735b3b0ff462b596a4d5e3d24e190360cd45245
NoahYannis <noah.guderjahn@gmx.de>
2023-09-11 06:44:45 +0200

    translations 2

--- PortfolioTrackerClient/Program.cs
+++ PortfolioTrackerClient/Program.cs
@@ -9,8 +9,11 @@ global using PortfolioTrackerClient.Services.GetStockInfoService;
 global using PortfolioTrackerClient.Services.PortfolioService;
 global using PortfolioTrackerShared.Models.UserModels;
 global using PortfolioTrackerShared.Other;
+global using Microsoft.Extensions.Localization;
 using PortfolioTrackerClient.Services.SettingsService;
-using Microsoft.Extensions.Localization;
+using System.Globalization;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Localization;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -23,11 +26,35 @@ builder.Services.AddScoped<IPortfolioService, PortfolioService>();
 builder.Services.AddScoped<IAuthService, AuthService>();
 builder.Services.AddScoped<ISettingsService, SettingsService>();
 
+
 builder.Services.AddBlazoredModal();
 builder.Services.AddOptions();
 builder.Services.AddAuthorizationCore();
 builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthStateProvider>();
 builder.Services.AddScoped<IStringLocalizer<App>, StringLocalizer<App>>();
+
+
+
+
+// Configure supported cultures
+var supportedCultures = new[]
+{
+    new CultureInfo("en-US"),
+    new CultureInfo("de-DE"),
+};
+
+// Create and configure the localizer
+builder.Services.AddLocalization(options => options.ResourcesPath = "Resources");
+builder.Services.Configure<RequestLocalizationOptions>(options =>
+{
+    options.DefaultRequestCulture = new RequestCulture("en-US");
+    options.SupportedCultures = supportedCultures;
+    options.SupportedUICultures = supportedCultures;
+});
+
+builder.Services.AddSingleton<IStringLocalizerFactory, ResourceManagerStringLocalizerFactory>();
+
+
 //builder.Services.AddLocalization();
 
 
