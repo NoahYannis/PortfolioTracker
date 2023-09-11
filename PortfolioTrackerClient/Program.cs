c6b6e6506943975d10e0be1fe55068cc8fcf70b1
Noah Yannis Guderjahn <77248099+NoahYannis@users.noreply.github.com>
2023-09-11 20:46:28 +0200

    added english resource strings

--- PortfolioTrackerClient/Program.cs
+++ PortfolioTrackerClient/Program.cs
@@ -10,13 +10,12 @@ global using PortfolioTrackerClient.Services.PortfolioService;
 global using PortfolioTrackerShared.Models.UserModels;
 global using PortfolioTrackerShared.Other;
 global using Microsoft.Extensions.Localization;
+global using PortfolioTrackerClient.Resources;
 using PortfolioTrackerClient.Services.SettingsService;
-using System.Globalization;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Localization;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
+builder.Services.AddLocalization();
 builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddBlazoredLocalStorage();
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
@@ -26,37 +25,9 @@ builder.Services.AddScoped<IPortfolioService, PortfolioService>();
 builder.Services.AddScoped<IAuthService, AuthService>();
 builder.Services.AddScoped<ISettingsService, SettingsService>();
 
-
 builder.Services.AddBlazoredModal();
 builder.Services.AddOptions();
 builder.Services.AddAuthorizationCore();
 builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthStateProvider>();
-builder.Services.AddScoped<IStringLocalizer<App>, StringLocalizer<App>>();
-
-
-
-
-// Configure supported cultures
-var supportedCultures = new[]
-{
-    new CultureInfo("en-US"),
-    new CultureInfo("de-DE"),
-};
-
-// Create and configure the localizer
-builder.Services.AddLocalization(options => options.ResourcesPath = "Resources");
-builder.Services.Configure<RequestLocalizationOptions>(options =>
-{
-    options.DefaultRequestCulture = new RequestCulture("en-US");
-    options.SupportedCultures = supportedCultures;
-    options.SupportedUICultures = supportedCultures;
-});
-
-builder.Services.AddSingleton<IStringLocalizerFactory, ResourceManagerStringLocalizerFactory>();
-
-
-//builder.Services.AddLocalization();
-
-
 
 await builder.Build().RunAsync();
