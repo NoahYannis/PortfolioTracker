142afe904fd79db2d6e33af3a03efcf91aee0dc6
Noah Yannis Guderjahn <77248099+NoahYannis@users.noreply.github.com>
2023-12-17 15:35:59 +0100

    responsive design with clamp function

--- PortfolioTrackerClient/obj/Debug/net7.0/PortfolioTrackerClient.AssemblyInfo.cs
+++ PortfolioTrackerClient/obj/Debug/net7.0/PortfolioTrackerClient.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PortfolioTrackerClient")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e17a1a13118bc3788523bf5ebcfd10cd7f3c1117")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+42c6904966349413756e12e22a3329e707567e62")]
 [assembly: System.Reflection.AssemblyProductAttribute("PortfolioTrackerClient")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PortfolioTrackerClient")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
