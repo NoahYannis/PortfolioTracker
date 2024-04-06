52229c39eace600e98354afdd69004e664e8851c
Noah Yannis Guderjahn <77248099+NoahYannis@users.noreply.github.com>
2024-04-06 09:56:11 +0200

    removed unused packages

--- PortfolioTrackerClient/obj/Debug/net8.0/PortfolioTrackerClient.AssemblyInfo.cs
+++ PortfolioTrackerClient/obj/Debug/net8.0/PortfolioTrackerClient.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PortfolioTrackerClient")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5330f48a2984b5a08d6eb830515204b0b471d7d6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d7b8ec6fe3f86cb1d6e47f1acffc79a9f9c37d47")]
 [assembly: System.Reflection.AssemblyProductAttribute("PortfolioTrackerClient")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PortfolioTrackerClient")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
