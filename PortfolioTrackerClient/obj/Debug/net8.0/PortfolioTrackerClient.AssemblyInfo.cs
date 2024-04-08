a240c351b301be7b5c9919f74f47c49d47fb475d
Noah Yannis Guderjahn <77248099+NoahYannis@users.noreply.github.com>
2024-04-08 21:44:21 +0200

    Adding PortfolioTrackerClient and Shared Project to the Dockerfile

--- PortfolioTrackerClient/obj/Debug/net8.0/PortfolioTrackerClient.AssemblyInfo.cs
+++ PortfolioTrackerClient/obj/Debug/net8.0/PortfolioTrackerClient.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PortfolioTrackerClient")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ac7e84e201dfe67bcf5c48080586d8983de74daf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d0e3b25a87bc62f64d4b0ae7f9f5af8397af7d00")]
 [assembly: System.Reflection.AssemblyProductAttribute("PortfolioTrackerClient")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PortfolioTrackerClient")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
